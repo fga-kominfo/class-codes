55cc86c0c87432be4fbcc5c2a8c99594ac64674b
arrizalrahmat <arrizalrahmat@gmail.com>
2024-09-02 20:27:44 +0700

    challenge

---
+++ sesi16/challenge.js
@@ -0,0 +1,27 @@
+class Car {
+  constructor() {
+    //property
+  }
+}
+
+class SUV extends Car {
+  constructor() {
+    super();
+  }
+}
+
+class CityCar extends Car {
+  constructor() {
+    super();
+  }
+}
+
+class Truck extends Car {
+  constructor() {
+    super();
+  }
+}
+
+class CarFactory {
+  static createCar() {}
+}
