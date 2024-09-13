754092243bf0a83407c28ba56e3231cf541a97ba
arrizalrahmat <arrizalrahmat@gmail.com>
2024-09-13 21:07:18 +0700

    sesi 25

--- sesi22/merge-promise.js
+++ sesi22/merge-promise.js
@@ -1,70 +1,70 @@
 const fs = require('fs').promises;
 
-// function getEmployees(cb) {
-//   Promise.all([
-//     fs.readFile('./employees.json', 'utf-8'),
-//     fs.readFile('./spouses.json', 'utf-8'),
-//     fs.readFile('./children.json', 'utf-8'),
-//   ])
-//     .then(([employeesData, spousesData, childrenData]) => {
-//       //proses ketika promise berhasil di resolve
-//       const {employees} = JSON.parse(employeesData);
-//       const {spouses} = JSON.parse(spousesData);
-//       const {children} = JSON.parse(childrenData);
+function getEmployees(cb) {
+  Promise.all([
+    fs.readFile('./employees.json', 'utf-8'),
+    fs.readFile('./spouses.json', 'utf-8'),
+    fs.readFile('./children.json', 'utf-8'),
+  ])
+    .then(([employeesData, spousesData, childrenData]) => {
+      //proses ketika promise berhasil di resolve
+      const {employees} = JSON.parse(employeesData);
+      const {spouses} = JSON.parse(spousesData);
+      const {children} = JSON.parse(childrenData);
 
-//       const mergedEmployees = employees.map(employee => {
-//         const spouse = spouses.find(
-//           spouse => spouse.employee_id === employee.id,
-//         );
+      const mergedEmployees = employees.map(employee => {
+        const spouse = spouses.find(
+          spouse => spouse.employee_id === employee.id,
+        );
 
-//         if (spouse) {
-//           employee.spouse = spouse;
-//         }
+        if (spouse) {
+          employee.spouse = spouse;
+        }
 
-//         const childrens = children.filter(
-//           child => child.employee_id === employee.id,
-//         );
+        const childrens = children.filter(
+          child => child.employee_id === employee.id,
+        );
 
-//         if (childrens.length) {
-//           employee.children = childrens;
-//         }
+        if (childrens.length) {
+          employee.children = childrens;
+        }
 
-//         return employee;
-//       });
-//       cb(mergedEmployees);
+        return employee;
+      });
+      cb(mergedEmployees);
 
-//       return fs.writeFile(
-//         './merged-employees2.json',
-//         JSON.stringify(mergedEmployees, null, 2),
-//       );
-//     })
-//     .then(() => {
-//       console.log('Merge Success');
-//     })
-//     .catch(err => {
-//       // error handling ketika promise di reject (gagal)
-//       console.log(err, '<<<err');
-//     })
-//     .finally(() => {
-//       console.log('Proses merge selesai');
-//     });
-// }
+      return fs.writeFile(
+        './merged-employees2.json',
+        JSON.stringify(mergedEmployees, null, 2),
+      );
+    })
+    .then(() => {
+      console.log('Merge Success');
+    })
+    .catch(err => {
+      // error handling ketika promise di reject (gagal)
+      console.log(err, '<<<err');
+    })
+    .finally(() => {
+      console.log('Proses merge selesai');
+    });
+}
 
-// const something = fs.readFileSync(
-//   './employees.json',
-//   'utf-8',
-// );
-// console.log(something);
+const something = fs.readFileSync(
+  './employees.json',
+  'utf-8',
+);
+console.log(something);
 
-// module.exports = getEmployees;
+module.exports = getEmployees;
 
-// fs.readFile('./employees.json', 'utf-8')
-//   .then(resEmployee => {
-//     const {employees} = JSON.parse(resEmployee);
-//     return fs.readFile('./spouses.json', 'utf-8');
-//   })
-//   .then(res => {
-//     const {spouses} = JSON.parse(res);
-//     console.log(resEmployee);
-//     console.log(spouses);
-//   });
+fs.readFile('./employees.json', 'utf-8')
+  .then(resEmployee => {
+    const {employees} = JSON.parse(resEmployee);
+    return fs.readFile('./spouses.json', 'utf-8');
+  })
+  .then(res => {
+    const {spouses} = JSON.parse(res);
+    console.log(resEmployee);
+    console.log(spouses);
+  });
--- sesi23/challenge/getPokeApi.js
+++ sesi23/challenge/getPokeApi.js
@@ -1,6 +1,63 @@
 const api = 'https://pokeapi.co/api/v2/pokemon/';
 
-function getPokemons() {}
+async function getPokemonsSynchronous() {
+  try {
+    // get list pokemons
+    const res = await fetch(api);
+    const {results} = await res.json();
+    const pokemons = [];
+
+    // get pokemon details
+    for (let i = 0; i < results.length; i++) {
+      const pokemon = results[i];
+      const {url} = pokemon;
+      const res = await fetch(url);
+      const detail = await res.json();
+
+      pokemon.detail = detail;
+
+      debugger;
+      pokemons.push(pokemon);
+    }
+
+    // save pokemon list to json
+    // await fs.writeFile(
+    //   './pokemons2.json',
+    //   JSON.stringify(pokemons, null, 2),
+    // );
+    console.log('pokemons json successfully created');
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// async function getPokemonsPromiseAll() {
+//   try {
+//     const res = await fetch(api);
+//     const {results} = await res.json();
+
+//     const pokemons = await Promise.all(
+//       results.map(async (pokemon, i) => {
+//         const res = await fetch(pokemon.url);
+//         const detail = await res.json();
+//         return {
+//           ...pokemon,
+//           detail,
+//         };
+//       }),
+//     );
+
+//     await fs.writeFile(
+//       './pokemons3.json',
+//       JSON.stringify(pokemons, null, 2),
+//     );
+//     console.log('completed');
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+getPokemonsSynchronous();
 
 // hasilnya => ditulis ke dalam pokemons.json
 /*
@@ -14,3 +71,28 @@ function getPokemons() {}
         }
     ]
 */
+
+// cosnt something = 'aku'
+
+// const nama = 'ari';
+// console.log(nama.first.last);
+
+// function isEven(num) {
+//   if (num % 2 === 0) return false;
+//   return true;
+// }
+
+// console.log(isEven(8));
+// console.log(isEven(7));
+
+// const firstName = 'arrizal';
+// let myVar = 1;
+// myVar++;
+// myVar++;
+// myVar++;
+// debugger;
+// myVar++;
+// debugger;
+// myVar++;
+
+// console.log(myVar);
